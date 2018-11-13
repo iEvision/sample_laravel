9b334e391eb0c67ffe8a0f3fc1ff676e2188abe3
Evit <1226847936@qq.com>
2018-11-13 17:34:05 +0800

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'Laravel About')
+
+@section('content')
+ <h1> About </h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', 'Laravel Help')
+
+@section('content')
+ <h1> Help</h1>
+@stop
